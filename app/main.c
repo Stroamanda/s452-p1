1ccf6fa76019207a63000a33e4789823b934e591
Amanda Stroud <114525992+Stroamanda@users.noreply.github.com>
2024-09-24 21:07:32 +0000

    Completed up to task 6

--- app/main.c
+++ app/main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../src/lab.h"
+#include "../src/lab.c"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <pwd.h>
+#include <string.h>
 
-int main(void)
-{
-  printf("hello world\n");
+int main(int argc, char *argv[]) {
+  int opt;
+  char cwd[1024];
+  char *prompt;
+  
+  prompt = strcat(strcat(getcwd(cwd, sizeof(cwd)), " "), get_prompt(NULL));
+
+  while((opt = getopt(argc, argv, "v")) != -1) {
+    switch(opt) {
+      case 'v':
+        printf("Version %d\n", lab_VERSION_MAJOR);
+        exit(0);
+        break;
+    }
+  }
+
+  char *line;
+  const char *changeDir = "cd ";
+  using_history();
+
+  while ((line=readline(prompt)) != NULL) {
+    printf("%s\n",line);
+    if (strcmp(line, "exit") == 0) {
+      free(line);
+      exit(0);
+    } else if (strncmp(line, "cd ", strlen(changeDir)) == 0 || strcmp(line, "cd") == 0) {
+      change_dir(&line);
+    } else if (strcmp(line, "history") == 0) {
+      for (int i = 0; i < history_length; i++) {
+          printf("%s\n", history_list()[i]->line);
+      }
+    } else {
+       printf("Unknown Command\n");
+    }
+
+    prompt = strcat(strcat(getcwd(cwd, sizeof(cwd)), " "), get_prompt(NULL));
+
+    add_history(line);
+    free(line);
+  }
+
+
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,42 @@
 /**Update this file with the starter code**/
+#include "../src/lab.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <string.h>
+
+  char *get_prompt(const char *env) {
+      char* prompt;
+
+      prompt = getenv("MY_PROMPT");
+
+      if (prompt == NULL) {
+        prompt = "$ ";
+      }
+
+      return prompt;
+  }
+
+  int change_dir(char **dir) {
+    char *direct = "";
+     if (strcmp(*dir, "cd") == 0) {
+      if (getenv("HOME") == NULL) {
+        direct = getpwuid(getuid())->pw_dir;
+        chdir(direct);
+      } else {
+        direct = getenv("HOME");
+        chdir(direct);
+      }
+     } else if (strncmp(*dir, "cd ", strlen("cd ")) == 0) {
+        if (strchr(*dir + strlen("cd "), ' ') == NULL) {
+          direct = *dir + strlen("cd ");
+
+          if (chdir(direct) != 0) {
+            printf("Couldn't Find Directory\n");
+          } 
+        } else {
+          printf("Couldn't Find Directory\n");
+        }
+     }
+    return 0;
+  }
