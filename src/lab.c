e1513647d52e78b6c9a4ed29801f132c788e9d8b
Amanda Stroud <114525992+Stroamanda@users.noreply.github.com>
2024-11-28 22:37:23 +0000

    sdfsdf

--- src/lab.c
+++ src/lab.c
@@ -219,7 +219,9 @@ void buddy_init(struct buddy_pool *pool, size_t size) {
         pool->avail[i].kval = i;
         pool->avail[i].tag = BLOCK_UNUSED;
 
-        fprintf(stderr, "avail[%zu]: tag=%d, kval=%zu\n", i, pool->avail[i].tag, pool->avail[i].kval);
+        fprintf(stderr, "avail[%u]: next = %p, prev = %p, kval = %u, tag = %d\n", 
+                i, pool->avail[i].next, pool->avail[i].prev, pool->avail[i].kval, pool->avail[i].tag);
+
     }
 
     pool->avail[pool->kval_m].next = pool->base;
